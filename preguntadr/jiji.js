0ffde6634b5087f6515c4aff96d6017702365edc
rocio salazar <rocio@gmail.com>
2024-02-20 10:26:08 -0300

    primera version jejej

---
+++ preguntadr/jiji.js
@@ -0,0 +1,3 @@
+//multiples de 3 fizz
+//multiples de 5 buzz
+// multiples de de 3 y 5 fizzbuzz
---
+++ preguntadr/script.js
@@ -0,0 +1,39 @@
+/* const nombre = "arocio"
+const edad = 25
+
+const puede_votar = edad > 18  */  
+
+// 1. le pregunto al usuario como se llama 
+
+/*const nombre = prompt("hola ¿como te llamas?")
+
+
+// 2. Creo un nuevo string con el saludo 
+const saludo =  " Hola, " + nombre + ", gusto conocerte"
+
+// 3. le mostramos el saludo al usuario 
+alert(saludo)*/
+
+// 1.Preguntarle al usuario por un numero
+let usuario = prompt("elige un numero") 
+
+// 1,5 transformar este string a numero 
+usuario = parseInt(usuario)
+
+//2.Sumarle 1 
+const  programa =  usuario + 1
+
+//3. Creamos la frase ganadora 
+const frase = "yo gané con el " + programa 
+
+//4. Le avisamos al usuario que perdio 
+alert(frase)
+
+
+let usuario =prompt("deja tu edad")
+
+const edad 
+
+const puede_votar = edad > 18
+
+const hacer_fila 
